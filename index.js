e87ba3e76933920cb71d652d79f6db23369d3ba6
Gexein <faust14_88@mail.ru>
2025-03-12 07:06:28 +0300

    Начинаем

---
+++ index.js
@@ -0,0 +1,44 @@
+const addTaskForm = document.querySelector('.todo__flex-element-add form');
+const select = document.querySelector('form select')
+const highBlock = document.querySelector('.todo__high')
+const mediumBlock = document.querySelector('.todo__medium')
+const lowBlock = document.querySelector('.todo__low')
+// const defaultInputValue = ''
+let toDoList = [
+    { task: 'Побрить ноги', priority: 'low' },
+    { task: 'Проветрить шкаф', priority: 'low' },
+    { task: 'Приготовить лягушачьи лапки', priority: 'medium' },
+    { task: 'Доказать всем, что ты хороший', priority: 'medium' },
+    { task: 'Сходить на концерт Газманова', priority: 'high' },
+    { task: 'Поспать', priority: 'high' }
+
+]
+
+
+function addTask(event) {
+    event.preventDefault();
+    const taskInput = document.querySelector('form input')
+    let taskInputValue = taskInput.value
+    let blockSample = `<div class="checkbox__task">
+                    <div class="checkbox__container">
+                    <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
+                        <input type="checkbox" id="task">
+                        <div class="todo__task"> ${taskInputValue}
+                        </div>
+                    </div>
+                    <button type="button">Удалить задачу</button>
+                </div>`
+    if (select.value === 'low') {
+        lowBlock.insertAdjacentHTML('beforeend', blockSample)
+    }
+    else if (select.value === 'medium') {
+        mediumBlock.insertAdjacentHTML('beforeend', blockSample)
+    }
+    else {
+        highBlock.insertAdjacentHTML('beforeend', blockSample)
+    }
+    event.target.reset()
+}
+
+
+addTaskForm.addEventListener('submit', addTask)

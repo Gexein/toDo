eed91f7244e647cd1971bdfdff25eba038aa687d
Gexein <faust14_88@mail.ru>
2025-03-12 15:45:05 +0300

    Почти все готов

--- index.js
+++ index.js
@@ -3,62 +3,20 @@ const select = document.querySelector('form select')
 const highBlock = document.querySelector('.todo__high')
 const mediumBlock = document.querySelector('.todo__medium')
 const lowBlock = document.querySelector('.todo__low')
+const dialogDoneList = document.querySelector('.dialog__done-list')
 const defaultInputValue = ''
-let toDoList = [
-    { task: 'Побрить ноги', priority: 'low' },
-    { task: 'Проветрить шкаф', priority: 'low' },
-    { task: 'Приготовить лягушачьи лапки', priority: 'medium' },
-    { task: 'Доказать всем, что ты хороший', priority: 'medium' },
-    { task: 'Сходить на концерт Газманова', priority: 'high' },
-    { task: 'Поспать', priority: 'high' }
-]
-
+let toDoList = []
+let doneList = []
 const priorities = { HIGH: 'high', MEDIUM: 'medium', LOW: 'low' }
-
-
-// Это первая версия кода 
-
-// function addTask(event) {
-//     event.preventDefault();
-//     const taskInput = document.querySelector('form input')
-//     let taskInputValue = taskInput.value
-// let blockSample = `<div class="checkbox__task">
-//                     <div class="checkbox__container">
-//                     <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
-//                         <input type="checkbox" id="task">
-//                         <div class="todo__task"> ${taskInputValue}
-//                         </div>
-//                     </div>
-//                     <button type="button">Удалить задачу</button>
-//                 </div>`
-//     if (select.value === 'low') {
-//         lowBlock.insertAdjacentHTML('beforeend', blockSample)
-//     }
-//     else if (select.value === 'medium') {
-//         mediumBlock.insertAdjacentHTML('beforeend', blockSample)
-//     }
-//     else {
-//         highBlock.insertAdjacentHTML('beforeend', blockSample)
-//     }
-//     event.target.reset()
-// }
-
-// Это новая версия кода 
-for (const obj of toDoList) {
-    console.log(obj.task)
-}
 
 function addTask(event) {
     event.preventDefault();
-    // let blockSample = `<div class="checkbox__task">
-    //                 <div class="checkbox__container">
-    //                 <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
-    //                     <input type="checkbox" id="task">
-    //                     <div class="todo__task">` + toDoList.task.task + `</div>
-    //                 </div>
-    //                 <button type="button">Удалить задачу</button>
-    //             </div>`;
-
+    let allTasks = document.querySelectorAll('.checkbox__task')
+    if (allTasks !== null) {
+        for (element of allTasks) {
+            element.remove()
+        }
+    }
     const taskInput = document.querySelector('form input')
     let taskInputValue = taskInput.value
     if (select.value === 'low') {
@@ -70,46 +28,63 @@ function addTask(event) {
     else {
         toDoList.push({ task: taskInputValue, priority: priorities.HIGH })
     }
-
     event.target.reset()
     for (const obj of toDoList) {
-        if (obj.priority === 'low') {
-            lowBlock.insertAdjacentHTML('beforeend', `<div class="checkbox__task">
+        let blockSample = `<div class="checkbox__task">
                 <div class="checkbox__container">
                 <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
-                    <input type="checkbox" id="task">
+                    <input type="checkbox" id="task" onclick="changeStatus(event)">
                     <div class="todo__task">${obj.task}</div>
                 </div>
-                <button type="button">Удалить задачу</button>
-            </div>`)
+                <button type="button" class="delete" onclick="deleteTask(event)">Удалить задачу</button>
+            </div>`;
+        if (obj.priority === 'low') {
+            lowBlock.insertAdjacentHTML('beforeend', blockSample)
         }
         else if (obj.priority === 'medium') {
-            mediumBlock.insertAdjacentHTML('beforeend', `<div class="checkbox__task">
-            <div class="checkbox__container">
-            <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
-                <input type="checkbox" id="task">
-                <div class="todo__task">${obj.task}</div>
-            </div>
-            <button type="button">Удалить задачу</button>
-        </div>`)
+            mediumBlock.insertAdjacentHTML('beforeend', blockSample)
         }
         else {
-            highBlock.insertAdjacentHTML('beforeend', `<div class="checkbox__task">
-            <div class="checkbox__container">
-            <div class="checkbox__tip-left">Нажмите сюда, если задача выполнена</div>
-                <input type="checkbox" id="task">
-                <div class="todo__task">${obj.task}</div>
-            </div>
-            <button type="button">Удалить задачу</button>
-        </div>`)
+            highBlock.insertAdjacentHTML('beforeend', blockSample)
         }
     }
-
 }
-function deleteTask(event) {
-
-}
-
 
 addTaskForm.addEventListener('submit', addTask)
+
+function deleteTask(event) {
+    const neededTextContent = String(event.target.parentElement.childNodes[1].childNodes[5].textContent)
+    const neededPriority = String(event.target.parentElement.parentElement.id);
+    toDoList.forEach((obj) => {
+        if (obj.task === neededTextContent && obj.priority === neededPriority) {
+            let neededIndex = toDoList.indexOf(obj)
+            toDoList.splice(neededIndex, 1)
+        }
+    })
+    event.target.parentElement.remove()
+}
+
+function changeStatus(event) {
+    const neededTextContent = String(event.target.parentElement.childNodes[5].textContent)
+    const neededPriority = String(event.target.parentElement.parentElement.parentElement.id);
+
+    toDoList.forEach((obj) => {
+        if (obj.task === neededTextContent && obj.priority === neededPriority) {
+            let neededIndex = toDoList.indexOf(obj)
+            let cutterArray = toDoList.splice(neededIndex, 1)
+            doneList = doneList.concat(cutterArray)
+        }
+    })
+
+    dialogDoneList.insertAdjacentHTML('afterbegin', `<li class="dialog__done-item">${neededTextContent}</li>`)
+    event.target.parentElement.parentElement.remove()
+}
+
+
+
+
+
+
+
+
 
